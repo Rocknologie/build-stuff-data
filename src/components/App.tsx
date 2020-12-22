f56dbc6884606e825e61a9484e5e480ff07e55a8
Rocknologie <rocknologie@hotmail.fr>
2020-12-22 11:38:32 +0100

    Init project

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './../css/tailwind.css';
+
+const App: React.FC = () => (
+  <></>
+)
+
+export default App;
